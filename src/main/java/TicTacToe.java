7eb048e10143c8fe43cd73b835b6efbfc5f9fab2
kirill <kirillgolan@mail.ru>
2022-02-18 21:21:19 +0300

    HomeWork4 is done

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -94,14 +94,6 @@ class TicTacToe {
     }
 
     boolean chekWin(char ch) {
-        //if (table[0][0] == ch && table[0][1] == ch &&table[0][2] == ch ) return  true;
-        //if (table[1][0] == ch && table[1][1] == ch &&table[1][2] == ch ) return  true;
-        //if (table[2][0] == ch && table[2][1] == ch &&table[2][2] == ch ) return  true;
-        //
-        //if (table[0][0] == ch && table[1][0] == ch &&table[2][0] == ch ) return  true;
-        //if (table[0][1] == ch && table[1][1] == ch &&table[2][1] == ch ) return  true;
-        //if (table[0][2] == ch && table[1][2] == ch &&table[2][2] == ch ) return  true;
-        //
         if (table[0][0] == ch && table[1][1] == ch &&table[2][2] == ch ) return  true;
         if (table[2][0] == ch && table[1][1] == ch &&table[0][2] == ch ) return  true;
         //горизонтальные линии
